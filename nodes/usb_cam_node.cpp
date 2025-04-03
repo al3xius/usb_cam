ea7350aca52b15346e3ff3394564042353b44d13
Alexander Schalk <al3xius.yt@gmail.com>
2025-04-03 11:08:53 +0200

    add dynamic reconfiguration

--- nodes/usb_cam_node.cpp
+++ nodes/usb_cam_node.cpp
@@ -41,6 +41,10 @@
 #include <sstream>
 #include <std_srvs/Empty.h>
 #include <std_msgs/String.h>
+#include <vo_autoexpose/vo_autoexpose.h>
+#include <cv_bridge/cv_bridge.h>
+#include <dynamic_reconfigure/server.h>
+#include <usb_cam_auto_exposure/CameraSettingsConfig.h>
 
 namespace usb_cam {
 
@@ -49,16 +53,21 @@ class UsbCamNode
 public:
   // private ROS node handle
   ros::NodeHandle node_;
+  ExpGainController controller; 
 
   // shared image message
   sensor_msgs::Image img_;
   image_transport::CameraPublisher image_pub_;
 
+  // Dynamic reconfigure
+  dynamic_reconfigure::Server<usb_cam_auto_exposure::CameraSettingsConfig> server_;
+  dynamic_reconfigure::Server<usb_cam_auto_exposure::CameraSettingsConfig>::CallbackType f_;
+
   // parameters
   std::string video_device_name_, io_method_name_, pixel_format_name_, camera_name_, camera_info_url_, color_format_name_, custom_auto_exposure_parameter_ ;
   //std::string start_service_name_, start_service_name_;
   bool streaming_status_;
-  int image_width_, image_height_, framerate_, exposure_, focus_, current_custom_exposure_;
+  int image_width_, image_height_, framerate_, exposure_, focus_, current_custom_exposure_, current_gain_;
   bool autofocus_, custom_auto_exposure_;
   boost::shared_ptr<camera_info_manager::CameraInfoManager> cinfo_;
 
@@ -154,6 +163,10 @@ public:
         if (param_name == custom_auto_exposure_parameter_) {
           current_custom_exposure_ = static_cast<int>(value);
         }
+
+        if (param_name == "gain") {
+          current_gain_ = static_cast<int>(value);
+        }
       }
     }
   }
@@ -169,7 +182,6 @@ public:
     cam_.start_capturing();
     return true;
   }
-
 
   bool service_stop_cap( std_srvs::Empty::Request  &req, std_srvs::Empty::Response &res )
   {
@@ -233,6 +245,10 @@ public:
       cinfo_->setCameraInfo(camera_info);
     }
 
+    // Setup exposure controller with default values
+    // We'll update with real camera parameters after connection
+    // setupExposureController();
+
     // Create a publisher for camera connection status
     camera_status_pub_ = node_.advertise<std_msgs::String>("camera_status", 1, true);
     
@@ -252,6 +268,71 @@ public:
 
     // Try to start the camera
     initialize_camera();
+
+    // Set up dynamic reconfigure
+    f_ = boost::bind(&UsbCamNode::dynamicReconfigureCallback, this, _1, _2);
+    server_.setCallback(f_);
+  }
+
+  // New method to set up the exposure controller with camera parameters
+  void setupExposureController() {
+    // Get calibration values from parameters or use defaults
+    Vector5f calib;
+    std::vector<double> calib_values;
+    if (node_.getParam("calibration_values", calib_values) && calib_values.size() == 5) {
+      calib << calib_values[0], calib_values[1], calib_values[2], calib_values[3], calib_values[4];
+    } else {
+      ROS_WARN("Using default calibration values. For best results, provide 'calibration_values' parameter.");
+      calib << 0.0, 0.0, 0.0, 0.0, 0.0;
+    }
+
+    // Get focal length values
+    double F, focal_length_x, focal_length_y;
+    node_.param("focal_length", F, 2.0);
+    
+    // Check if we have calibrated camera info
+    if (cinfo_->isCalibrated()) {
+      sensor_msgs::CameraInfo info = cinfo_->getCameraInfo();
+      focal_length_x = info.K[0]; // fx is in the calibration matrix K[0]
+      focal_length_y = info.K[4]; // fy is in the calibration matrix K[4]
+      
+      // If no values in calibration matrix, try to get from parameters
+      if (focal_length_x == 0.0) {
+        node_.param("focal_length_x", focal_length_x, 865.909537);
+      }
+      if (focal_length_y == 0.0) {
+        node_.param("focal_length_y", focal_length_y, 865.909537);
+      }
+    } else {
+      // No calibration, use parameters or defaults
+      node_.param("focal_length_x", focal_length_x, 865.909537);
+      node_.param("focal_length_y", focal_length_y, 865.909537);
+    }
+
+    // Configure gamma values
+    VectorNcorrect gamma_values;
+    std::vector<double> gamma_params;
+    if (node_.getParam("gamma_values", gamma_params) && gamma_params.size() == gamma_values.size()) {
+      for (size_t i = 0; i < gamma_params.size(); i++) {
+        gamma_values[i] = gamma_params[i];
+      }
+    } else {
+      // gamma_values << 1.0/1.9, 1.0/1.5, 1.0/1.2, 1.0, 1.2, 1.5, 1.9;
+    }
+
+    gamma_values << 0, 1, 2, 3, 4, 5, 6;
+
+    // Get camera identification
+    std::string camera_id;
+    node_.param("camera_id", camera_id, std::string("left"));
+    
+    ROS_INFO("Configuring exposure controller: %s with dimensions %dx%d, F: %.1f, fx: %.1f, fy: %.1f", 
+             camera_id.c_str(), image_width_, image_height_, F, focal_length_x, focal_length_y);
+
+    // Add camera to controller
+    controller.add_camera(camera_id, image_height_, image_width_, FLT_DIG, focal_length_x, focal_length_y, calib);
+    controller.set_gamma_values(gamma_values);
+    controller.init();
   }
 
   // Method to publish camera status
@@ -325,6 +406,11 @@ public:
         
         // Reset reconnect delay when successfully connected
         reconnect_delay_ms_ = 500;
+        
+        // If we successfully connected, update the controller with current parameters
+        // if (camera_connected_) {
+        //   setupExposureController();
+        // }
         
         return true;
       } 
@@ -494,14 +580,53 @@ public:
     image_pub_.publish(img_, *ci);
 
     // Handle auto exposure if enabled
-    if (custom_auto_exposure_) {
-      frames_since_last_adjustment_++;
-      if (frames_since_last_adjustment_ >= auto_exposure_frames_skip_) {
-        frames_since_last_adjustment_ = 0;
-        double avg_brightness = calculateAverageBrightness(img_);
-        adjustExposure(avg_brightness);
-      }
-    }
+    // if (custom_auto_exposure_) {
+    //   // frames_since_last_adjustment_++;
+    //   // if (frames_since_last_adjustment_ >= auto_exposure_frames_skip_) {
+    //   //   frames_since_last_adjustment_ = 0;
+    //   //   double avg_brightness = calculateAverageBrightness(img_);
+    //   //   adjustExposure(avg_brightness);
+    //   // }
+
+    //   cv_bridge::CvImagePtr cv_ptr;
+    //   cv_ptr = cv_bridge::toCvCopy(img_,"mono8");
+
+    //   controller.add_frame(0,cv_ptr->image,float(current_custom_exposure_)/1000000,controller.factor_2_dB(current_gain_),cv_ptr->header.seq,cv_ptr->header.stamp.toSec());
+
+
+    //   // Get the new exposure and gain values
+    //   ExposureParameters new_exposure_params = controller.update_params(GRADINFO_SCORE,EV_CORRECTIONS,OPTIFLOW_ESTIM);
+
+    //   int new_gain = static_cast<int>(controller.dB_2_factor(new_exposure_params.gain));
+    //   // convert gain from db to range 0 to 255
+    //   new_gain = std::max(0, std::min(20, new_gain));
+
+    //   int new_exposure = static_cast<int>(1000000*new_exposure_params.exposure);
+    //   new_exposure = std::max(min_exposure_, std::min(max_exposure_, new_exposure));
+
+    //   // update parameters
+    //   if (new_exposure != current_custom_exposure_) {
+    //     ROS_INFO("Updating exposure to %d", new_exposure);
+    //     cam_.set_v4l_parameter(custom_auto_exposure_parameter_, new_exposure);
+    //     current_custom_exposure_ = new_exposure;
+    //   }
+
+    //   if (new_gain != current_gain_) {
+    //     ROS_INFO("Updating gain to %d", new_gain);
+    //     cam_.set_v4l_parameter("gain", new_gain);
+    //     current_gain_ = new_gain;
+    //   }
+    // }
+
+    // Update dynamic reconfigure with current values
+    // This ensures the GUI always shows the actual camera settings
+    // usb_cam::CameraSettingConfig config;
+    // config.exposure = current_custom_exposure_;
+    // config.gain = current_gain_;
+    // config.auto_exposure = custom_auto_exposure_;
+    // config.target_brightness = target_brightness_;
+    // config.brightness_tolerance = brightness_tolerance_;
+    // server_.updateConfig(config);
 
     return true;
   }
@@ -535,11 +660,24 @@ public:
 
         // if ((ros::Time::now() - last_reconnect_attempt) > reconnect_duration) {        
         //   ros::Duration reconnect_duration(reconnect_delay_ms_ / 1000.0);
-
       }
+      ros::spinOnce();
+      loop_rate.sleep();
     }
 
     return true;
+  }
+
+  // Dynamic reconfigure callback
+  void dynamicReconfigureCallback(usb_cam_auto_exposure::CameraSettingsConfig &config, uint32_t level) {
+    ROS_INFO("Dynamic reconfigure called with level %d", level);
+    
+    try {
+      cam_.set_v4l_parameter(custom_auto_exposure_parameter_, config.exposure);
+      current_custom_exposure_ = config.exposure;
+    } catch (std::exception &e) {
+      ROS_ERROR("Failed to set exposure: %s", e.what());
+    }    
   }
 };   
 }     
@@ -547,7 +685,7 @@ public:
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "usb_cam");
+  ros::init(argc, argv, "usb_cam_auto_exposure");
   usb_cam::UsbCamNode a;
   a.spin();
   return EXIT_SUCCESS;
